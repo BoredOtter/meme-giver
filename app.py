357c936488d3fdcd1e5f605093aaed1075e65120
unknown <eryk.stando@gmail.com>
2023-07-10 11:41:22 +0200

    app.py fix

--- app.py
+++ app.py
@@ -43,8 +43,7 @@ def meme():
 
 @app.errorhandler(404)
 def page_not_found(e):
-    # note that we set the 404 status explicitly
-    return render_template("404.html")
+    return render_template("404.html"), 404
 
 
 if __name__ == '__main__':
